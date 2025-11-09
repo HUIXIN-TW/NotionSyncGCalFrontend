6ea016cc3b3a87dab94156a6a3e79bc825f17e34
HUIXIN-TW <huixin.yang.tw@gmail.com>
2025-11-09 12:46:41 +0800

    style: prettier

--- src/components/callout/NewUserWelcomeSection.jsx
+++ src/components/callout/NewUserWelcomeSection.jsx
@@ -9,7 +9,10 @@ export default function NewUserWelcomeSection() {
       <div className={styles.calloutIcon}>👋</div>
       <div>
         <h2>Set up your Notion Sync Configuration</h2>
-        To start syncing, add your <strong>Basic - Notion Database ID</strong> and
+        To start syncing, add your <strong>
+          Basic - Notion Database ID
+        </strong>{" "}
+        and
         <strong> GCal - Google Calendar Mapping</strong>.
       </div>
     </div>
