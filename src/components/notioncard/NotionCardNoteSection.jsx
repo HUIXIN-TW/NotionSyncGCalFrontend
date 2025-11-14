7c2685b4e325dfce64afb58c3179ffea1097af5e
HUIXIN-TW <huixin.yang.tw@gmail.com>
2025-11-15 02:26:50 +0800

    feat: add client-side configuration for Notion templates and rate limits

--- src/components/notioncard/NotionCardNoteSection.jsx
+++ src/components/notioncard/NotionCardNoteSection.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styles from "./notioncard.module.css";
-import config from "@config/server/notion";
+import config from "@/config/client/notion";
 
 export default function NotionCardNoteSection({
   messages,
